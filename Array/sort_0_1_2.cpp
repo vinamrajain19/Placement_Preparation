e80928fc8085f659b123d39c4dd0b95778b36c89
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-03-17 17:04:14 +0530

    Create sort_0_1_2.cpp

---
+++ Array/sort_0_1_2.cpp
@@ -0,0 +1,26 @@
+
+// Dutch National flag Algorithmn
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int l = 0;
+        int m = 0;
+        int h = nums.size()-1;
+        
+        
+        while(m <= h){
+            if(nums[m] == 0){
+                swap(nums[m],nums[l]);
+                m++;
+                l++;
+            }
+            else if(nums[m] == 1) m++;
+            
+            else if(nums[m] == 2){
+                swap(nums[m],nums[h]);
+                h--;
+            }
+        }
+    }
+};
