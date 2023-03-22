897c35d67e556742ae1a122faf8c6ceeb6891a0a
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-03-22 17:18:10 +0530

    Create Max_consecutive _ones3.cpp

---
+++ Array/Max_consecutive _ones3.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int i = 0;
+        int j = 0;
+        
+        int ans = 0;
+        int cnt = 0;
+        
+        while(j < nums.size()){
+            if(nums[j] == 0) cnt++;
+            
+            while(cnt  > k and i < nums.size()){
+                if(nums[i] == 0){
+                    cnt--;
+                }
+                i++;
+            }
+            
+            ans = max(ans,j-i+1);
+            j++;
+        }
+        
+        return ans;
+    }
+};
